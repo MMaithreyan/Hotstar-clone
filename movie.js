fef156e1d3ebbdc08f0a823f5944bba10fb5b0a3
maithreyan <maithreyanm8@gmail.com>
2021-12-10 23:19:53 +0530

    coursel ADDED

---
+++ movie.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'Iron-Man',
+        des: 'American superhero film based on the Marvel Comics character of the same name In the film, following his escape from captivity by a terrorist group, world famousindustrialist and master engineer Tony Stark builds a mechanized suit of armor and becomes the superhero Iron Man.',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'loki',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 5.PNG'
+    }
+]
