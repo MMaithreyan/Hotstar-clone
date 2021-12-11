c99a51130a4a9141b520ae69cd471fb7223ea6a7
maithreyan <maithreyanm8@gmail.com>
2021-12-11 23:46:14 +0530

    images added

--- movie.js
+++ movie.js
@@ -2,26 +2,26 @@ let movies = [
     {
         name: 'Iron-Man',
         des: 'American superhero film based on the Marvel Comics character of the same name In the film, following his escape from captivity by a terrorist group, world famousindustrialist and master engineer Tony Stark builds a mechanized suit of armor and becomes the superhero Iron Man.',
+        image: 'images/slider 1.jpg'
+    },
+    {
+        name: 'Falcon and the Winter Soldier',
+        des: 'Falcon and the Winter Soldier are a mismatched duo who team up for a global adventure that will test their survival skills -- as well as their patience.',
         image: 'images/slider 2.PNG'
     },
     {
-        name: 'loki',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
-        image: 'images/slider 1.PNG'
-    },
-    {
-        name: 'wanda vision',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        name: 'Wanda Vision',
+        des: 'Living idealized suburban lives, super-powered beings Wanda and Vision begin to suspect that everything is not as it seems.',
         image: 'images/slider 3.PNG'
     },
     {
-        name: 'raya and the last dragon',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        name: 'Raya and the last dragon',
+        des: 'Raya, a warrior, sets out to track down Sisu, a dragon, who transferred all her powers into a magical gem which is now scattered all over the kingdom of Kumandra, dividing its people.',
         image: 'images/slider 4.PNG'
     },
     {
-        name: 'luca',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        name: 'Luca',
+        des: 'Set in a beautiful seaside town on the Italian Riviera, the original animated feature is a coming-of-age story about one young boy experiencing an unforgettable summer filled with gelato, pasta and endless scooter rides. Luca shares these adventures with his newfound best friend, but all the fun is threatened by a deeply held secret: he is a sea monster from another world just below the water',
         image: 'images/slider 5.PNG'
     }
 ]
