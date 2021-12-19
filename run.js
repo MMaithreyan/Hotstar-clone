ecf4d1fd99af39de1758218340e4878d5533c004
maithreyan <maithreyanm8@gmail.com>
2021-12-19 19:10:53 +0530

    images added

--- run.js
+++ run.js
@@ -70,20 +70,20 @@ videoCards.forEach(item => {
 
 
 
-let cardContainers = [document.querySelectorAll('.card-container')];
-let preBtns = [document.querySelectorAll('.pre-btn')];
-let nxtBtns = [document.querySelectorAll('.nxt-btn')];
+let cardContainers = [...document.querySelectorAll('.card-container')];
+let preBtns = [...document.querySelectorAll('.pre-btn')];
+let nxtBtns = [...document.querySelectorAll('.nxt-btn')];
 
 cardContainers.forEach((item, i) => {
     let containerDimensions = item.getBoundingClientRect();
     let containerWidth = containerDimensions.width;
 
     nxtBtns[i].addEventListener('click', () => {
-        item.scrollLeft;
+        item.scrollLeft += containerWidth - 200;
     })
 
     preBtns[i].addEventListener('click', () => {
-        item.scrollLeft;
+        item.scrollLeft += containerWidth + 200;
     })
 }
 )
